e47725ffe7aaeb474f03b080972f824eed6c78f9
Ricardo <rdobmk@gmail.com>
2021-03-12 20:22:27 +0100

    first commit of graphQL

---
+++ graphql/resolvers.js
@@ -0,0 +1,14 @@
+
+
+
+/// #### FIRST TEST OF GRAPHQL ####
+/* 
+module.exports = {
+    hello() {
+        return {
+            text: 'Hello World',
+            views: 1245
+        };
+    }
+};
+ */
---
+++ graphql/schema.js
@@ -0,0 +1,57 @@
+const { buildSchema } = require('graphql');
+
+module.exports = buildSchema(`
+    type Post {
+        _id: ID!
+        title: String!
+        content: String!
+        imageUrl: String!
+        creator: User!
+        createAt: String!
+        updatedAt: String!
+    }
+
+    type User {
+        _id: ID!,
+        name: String!,
+        email: String!,
+        status: String,
+        posts: [Post!]
+    }
+
+    input UserInputData {
+        email: String!
+        name: String!
+        password: String!
+    }
+
+    type RootMutation {
+        createUser(userInput: UserInputData): User!
+    }
+
+    schema {
+        mutation: RootMutation
+    }
+`);
+
+
+
+
+/// #### FIRST TEST OF GRAPHQL ####
+/* 
+module.exports = buildSchema(`
+    type TestData {
+        text: String!
+        views: Int!
+    }
+
+    type RootQuery {
+        hello: TestData!
+    }
+
+    schema {
+        query: RootQuery
+    }
+`);
+
+ */
