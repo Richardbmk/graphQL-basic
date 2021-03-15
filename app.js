fb98b15c4fc3f8d5104f251333dce15bc9c071c2
Ricardo <rdobmk@gmail.com>
2021-03-15 20:13:09 +0100

    signup added

--- app.js
+++ app.js
@@ -50,6 +50,9 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
     next();
 });
 
@@ -57,7 +60,17 @@ app.use(
     '/graphql',
     graphqlHTTP({
         schema: graphqlSchema,
-        rootValue: graphqlResolver
+        rootValue: graphqlResolver,
+        graphiql: true,
+        formatError(err) {
+            if (!err.originalError) {
+                return err;
+            }
+            const data = err.originalError.data;
+            const message = err.message || 'An error occurred.';
+            const code = err.originalError.code || 500;
+            return { message: message, status: code, data: data }
+        }
     })
 );
 
